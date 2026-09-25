baf0655ffe7600b75ff8ee90a5ffecf1e114a2ee
ajcanepa <ajcanepa@ubu.es>
2026-09-25 10:43:31 +0200

    ggplot2 art

--- 06_VISUALIZACION_INFORMACION/Art_ggplot2.R
+++ 06_VISUALIZACION_INFORMACION/Art_ggplot2.R
@@ -1,6 +1,6 @@
 #library(tidyverse)
 # CIRCULAR ----------------------------------------------------------------
-n = 10
+n = 100
 #n = 100
 #n = 500
 t1 = 1:n
@@ -20,6 +20,63 @@ seq(from = -10, to = 10, by = 0.1) %>%
   ggplot(aes(x = (x + pi*sin(y)), y = (y + pi*sin(x)))) +
   geom_point(alpha = .1, shape = 20, size = 1, color = "black") +
   theme_void()
+
+
+# CORAZON -----------------------------------------------------------------
+# 1. Corazón en coordenadas cartesianas
+#    Curva paramétrica:  x = 16 sin^3(t)
+#                        y = 13 cos(t) - 5 cos(2t) - 2 cos(3t) - cos(4t)
+
+corazon <- tibble(t = seq(0, 2 * pi, length.out = 500)) |>
+  mutate(x = 16 * sin(t)^3,
+         y = 13 * cos(t) - 5 * cos(2 * t) - 2 * cos(3 * t) - cos(4 * t))
+
+ggplot(corazon, aes(x, y)) +
+  geom_hline(yintercept = 0, colour = "grey50") +
+  geom_vline(xintercept = 0, colour = "grey50") +
+  geom_polygon(fill = "red3", alpha = 0.8) +
+  coord_equal() +
+  theme_minimal()
+
+# 1b. Variante: el corazón "de la fórmula viral" (una sola función y = f(x))
+#     y = |x|^(2/3) + 0.9 * sqrt(3.3 - x^2) * sin(a * pi * x)
+#     Cuanto mayor es 'a', más denso se llena el corazón.
+tibble(x = seq(-sqrt(3.3), sqrt(3.3), length.out = 3000)) |>
+  mutate(y = abs(x)^(2/3) + 0.9 * sqrt(3.3 - x^2) * sin(20 * pi * x)) |>
+  ggplot(aes(x, y)) +
+  geom_line(colour = "red3") +
+  coord_equal() +
+  theme_minimal()
+
+
+# GIRASOL -----------------------------------------------------------------
+# 2. Girasol (filotaxis): cada semilla gira el "ángulo áureo"
+#    radio = sqrt(i),  ángulo = i * pi * (3 - sqrt(5))  (~137.5 grados)
+tibble(i = 1:1500) |>
+  mutate(x = sqrt(i) * cos(i * pi * (3 - sqrt(5))),
+         y = sqrt(i) * sin(i * pi * (3 - sqrt(5)))) |>
+  ggplot(aes(x, y, colour = i, size = i)) +
+  geom_point(show.legend = FALSE) +
+  scale_colour_viridis_c(option = "inferno") +
+  scale_size(range = c(0.3, 3)) +
+  coord_equal() +
+  theme_void() +
+  theme(plot.background = element_rect(fill = "black"))
+
+
+# ESPIRAL AUREA -----------------------------------------------------------
+phi <- (1 + sqrt(5)) / 2   # número áureo
+
+tibble(theta = seq(-2 * pi, 4 * pi, length.out = 2000)) |>
+  mutate(r = phi^(2 * theta / pi),
+         x = r * cos(theta),
+         y = r * sin(theta)) |>
+  ggplot(aes(x, y)) +
+  geom_hline(yintercept = 0, colour = "grey50") +
+  geom_vline(xintercept = 0, colour = "grey50") +
+  geom_path(colour = "darkorange", linewidth = 1) +
+  coord_equal() +
+  theme_minimal()
 
 
 # FLUID -------------------------------------------------------------------
